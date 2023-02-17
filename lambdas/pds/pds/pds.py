dcbc89ee1b161ec5fe00961a70154348e06da4af
hasnat.abdul1 <hasnat.abdul1@nhs.net>
2023-02-17 16:55:35 +0000

    Update pds.py for better handling

--- lambdas/pds/pds/pds.py
+++ lambdas/pds/pds/pds.py
@@ -8,30 +8,43 @@ SANDBOX_URL = "https://sandbox.api.service.nhs.uk/personal-demographics/FHIR/R4"
 
 
 def check_nhs_number(nhs_number):
+    """Check NHS Number"""
     return is_valid(str(nhs_number))
 
 
 def get_pds_patient_data(nhs_number):
     """Retirieve JSON Body from PDS API"""
     headers = {"X-Request-ID": str(uuid.uuid4())}
-    pds_response =  requests.get(f"{SANDBOX_URL}/Patient/{nhs_number}", headers=headers)
-    return json.loads(pds_response.text)
+
+    return requests.get(f"{SANDBOX_URL}/Patient/{nhs_number}", headers=headers)
 
 
-def get_ods_code(nhs_number):
+def get_ods_code(body):
     """Extract ODS Code from PDS Patient Response"""
-    return get_pds_patient_data(nhs_number)["generalPractitioner"][0]["identifier"]["value"]
+    return body["generalPractitioner"][0]["identifier"]["value"]
 
 
 def handler(event, _context) -> Dict:
     """Invoke PDS Lambda"""
     nhs_number = event["nhs_number"]
 
+    is_valid_nhs_number = is_valid(str(nhs_number))
+    if is_valid_nhs_number:
+        respone = get_pds_patient_data(nhs_number)
+
+        status_code = respone.status_code
+
+        body = json.loads(respone.text)
+        ods_code = get_ods_code(body)
+
+    else:
+        status_code = 404
+        ods_code = ""
 
     return {
-        "statusCode": 200,
+        "statusCode": status_code,
         "body": {
-            "result": get_ods_code(nhs_number),
-            "valid_nhs": check_nhs_number(nhs_number)
+            "result": ods_code,
+            "valid_nhs": is_valid_nhs_number,
         },
     }
