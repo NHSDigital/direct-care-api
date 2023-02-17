780fc2e9d54b871d093d8e1f14394da9eaaa7111
hasnat.abdul1 <hasnat.abdul1@nhs.net>
2023-02-17 10:51:23 +0000

    add nhs number check

--- lambdas/pds/pds/pds.py
+++ lambdas/pds/pds/pds.py
@@ -2,25 +2,29 @@ from typing import Dict
 import uuid
 import json
 import requests
+from nhs_number import is_valid
 
 SANDBOX_URL = "https://sandbox.api.service.nhs.uk/personal-demographics/FHIR/R4"
 
 
 def check_nhs_number(nhs_number):
-    pass
+    return is_valid(str(nhs_number))
+
 
 def get_pds_patient_data(nhs_number):
+    """Retirieve JSON Body from PDS API"""
     headers = {"X-Request-ID": str(uuid.uuid4())}
     pds_response =  requests.get(f"{SANDBOX_URL}/Patient/{nhs_number}", headers=headers)
     return json.loads(pds_response.text)
 
+
 def get_ods_code(nhs_number):
+    """Extract ODS Code from PDS Patient Response"""
     return get_pds_patient_data(nhs_number)["generalPractitioner"][0]["identifier"]["value"]
 
 
-
 def handler(event, _context) -> Dict:
-    """Invoke pds"""
+    """Invoke PDS Lambda"""
     nhs_number = event["nhs_number"]
 
 
@@ -28,5 +32,6 @@ def handler(event, _context) -> Dict:
         "statusCode": 200,
         "body": {
             "result": get_ods_code(nhs_number),
+            "valid_nhs": check_nhs_number(nhs_number)
         },
     }
