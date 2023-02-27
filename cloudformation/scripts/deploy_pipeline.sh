fbbbd0d429f72a16fe63af79ec330df60701189d
Anthony Brown <anthony.brown8@nhs.net>
2023-02-27 11:11:53 +0000

    export codepipeline name

--- cloudformation/scripts/deploy_pipeline.sh
+++ cloudformation/scripts/deploy_pipeline.sh
@@ -4,7 +4,7 @@ pull_request_id=$1
 FeatureGitBranch=$2
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-
+# get dev information
 cloudformation_exports=`aws cloudformation list-exports --profile nhs-direct-care-dev`
 
 devPipelineExecutionRole=`echo $cloudformation_exports | jq -r '.Exports[] |select(.Name=="aws-sam-cli-managed-dev-pipeline-resources:PipelineExecutionRole").Value'`
@@ -13,7 +13,7 @@ devArtifactsBucketARN=`echo $cloudformation_exports | jq -r '.Exports[] |select(
 arr_devArtifactsBucketARN=(${devArtifactsBucketARN//:/ })
 devArtifactsBucket=${arr_devArtifactsBucketARN[3]}
 
-
+# get int information
 cloudformation_exports=`aws cloudformation list-exports --profile nhs-direct-care-int`
 
 intPipelineExecutionRole=`echo $cloudformation_exports | jq -r '.Exports[] |select(.Name=="aws-sam-cli-managed-int-pipeline-resources:PipelineExecutionRole").Value'`
@@ -44,7 +44,7 @@ echo "FeatureGitBranch               : $FeatureGitBranch"
 echo "Testing StackName              : $TestingStackName"
 echo "Prod StackName                 : $ProdStackName"
 
-
+# fix template params file for stack deployment
 cp $SCRIPT_DIR/../resources/ci_pipeline_params_template.json /tmp/ci_pipeline_params.json
 sed -i "s#@devPipelineExecutionRole#$devPipelineExecutionRole#g" /tmp/ci_pipeline_params.json
 sed -i "s#@devArtifactsBucket#$devArtifactsBucket#g" /tmp/ci_pipeline_params.json
@@ -65,11 +65,19 @@ sed -i "s#@ProdStackName#$ProdStackName#g" /tmp/ci_pipeline_params.json
 #		--no-execute-changeset \
 #		--no-fail-on-empty-changeset
 
+# check if stack exists
+if aws cloudformation describe-stacks --profile nhs-direct-care-pipelines \
+	--stack-name  ${stackName}  &>/dev/null 
+then
+    echo 'stack exists'
+else
+	echo 'stack does not exist'
+fi
 
-aws cloudformation deploy \
-		--profile nhs-direct-care-pipelines \
-		--template-file ${SCRIPT_DIR}/../resources/ci_pipeline.yaml \
-		--stack-name ${stackName} \
-		--parameter-overrides file:///tmp/ci_pipeline_params.json \
-		--capabilities CAPABILITY_IAM \
-		--no-fail-on-empty-changeset
+#aws cloudformation deploy \
+#		--profile nhs-direct-care-pipelines \
+#		--template-file ${SCRIPT_DIR}/../resources/ci_pipeline.yaml \
+#		--stack-name ${stackName} \
+#		--parameter-overrides file:///tmp/ci_pipeline_params.json \
+#		--capabilities CAPABILITY_IAM \
+#		--no-fail-on-empty-changeset
