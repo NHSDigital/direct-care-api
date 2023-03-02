95bb4b70c1bef4a1f6763ccdfe947a471109836b
Ed Bright <ed.feldmanbright@nhs.net>
2023-03-02 15:37:02 +0000

    Add missing lib files

---
+++ lambdas/orchestration/lib/absolute_file_path.py
@@ -0,0 +1,16 @@
+"""
+When the lambda function is created (see package-lambdas command in Makefile)
+the directory structure changes slightly so whenever you need to access a file in
+the python code, you need to get the absolute path of the *python* file that wants to use
+that file, then navigate to the file relatively
+"""
+
+import os
+
+
+def absolute_file_path(root, *paths):
+    """Get the absolute file path"""
+    return os.path.join(
+        os.path.dirname(os.path.realpath(root)),
+        *paths,
+    )
---
+++ lambdas/orchestration/lib/write_log.py
@@ -0,0 +1,35 @@
+import os
+
+from .absolute_file_path import absolute_file_path
+from .get_logbase import LogbaseError, parse_log_base
+
+LOG_CONFIG = parse_log_base(os.path.join(absolute_file_path(__file__), "logbase.cfg"))
+
+
+class LogRefNotFound(LogbaseError):
+    """Exception to raise if attempted log is not in logbase"""
+
+
+class MissingVariables(LogbaseError):
+    """Exception to raise if incomplete log dict passed to log"""
+
+
+def write_log(log_ref, log_dict):
+
+    # Find log
+    log = LOG_CONFIG.get(log_ref)
+    if not log:  # pragma: no cover
+        raise LogRefNotFound(f"Log with Ref={log_ref} not found in logbase")
+
+    # Level
+    level = log["level"]
+
+    # Text
+    try:
+        log_text = log["text"]
+        text = log_text.format(**log_dict)
+    except KeyError as exc:  # pragma: no cover
+        raise MissingVariables(
+            f"Missing variables passed to log with text={log_text} log_dict={log_dict}"
+        ) from exc
+    print(f"Ref={log_ref} Level={level} {text}")
