58cf4a24d3d16e8878611e5766cd129799c8b0a3
Natasa Fragkou <natasa.fragkou@nhs.net>
2023-03-06 17:24:31 +0000

    adds None param in args

--- lambdas/orchestration/app/lib/make_request.py
+++ lambdas/orchestration/app/lib/make_request.py
@@ -1,9 +1,9 @@
 import requests
 
 
-def make_get_request(url, headers):
+def make_get_request(url, headers, params=None):
     """Extracted function to make mocking requests easier"""
-    return requests.get(url, headers=headers, timeout=600)
+    return requests.get(url, headers=headers, params=params, timeout=600)
 
 
 def make_post_request(url, data, headers):
