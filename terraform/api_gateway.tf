68d7507e43fe154c3ea737ac8ff59feb175234d5
Ed Bright <ed.feldmanbright@nhs.net>
2023-03-11 14:13:46 +0000

    Don't force redeploy on api gateway

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -8,12 +8,6 @@ resource "aws_api_gateway_deployment" "api_gateway_deployment" {
   ]
   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
 
-
-  # Always redeploy
-  triggers = {
-    redeployment = timestamp()
-  }
-
   lifecycle {
     create_before_destroy = true
   }
